9119cb8047734e7c40afdf6eea21871e9103d8af
Labidi90 <labidimohamed90@gmail.com>
2022-07-23 02:02:45 +0200

    checkpoint done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import Timer from "./components/Timer";
+import img from "./img.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      imgSrc: img,
+      fullname: "Abidi Mohamed",
+      profession: "Fullsatck Developer",
+      bio: "Recent computer science graduate with a passion for developing scalable web applications and working across the full stack. I am looking to join forces with Red Technologies to continue to grow my skill set while contributing to the positive outcome of making people richer smarter and happier.",
+    },
+  };
+
+  toggleVisibility = () => {
+    this.setState({ isVisible: !this.state.isVisible });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+
+        <button className="btn_primary" onClick={this.toggleVisibility}>
+          {this.state.isVisible ? "Hide Profile" : "Show Profile"}
+        </button>
+        {this.state.isVisible && (
+          <Timer
+            imgSrc={this.state.person.imgSrc}
+            fullname={this.state.person.fullname}
+            profession={this.state.person.profession}
+            bio={this.state.person.bio}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Timer.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        imgSrc: "set profile picture",
+        fullname: "write ur name",
+        profession: "write down ur profession",
+        bio: "tell us about urself",
+      },
+
+      timer: 0,
+    };
+    console.log("run constructor");
+  }
+
+  componentDidMount = () => {
+    console.log("run componentDidMount");
+    this.intervalId = setInterval(() => {
+      this.setState({ timer: this.state.timer + 1 });
+    }, 1000);
+  };
+
+  componentDidUpdate = () => {
+    console.log("run componentDidUpdate");
+  };
+
+  componentWillUnmount = () => {
+    console.log("run componentWillUnmount");
+    clearInterval(this.intervalId);
+  };
+
+  render() {
+    console.log("run render");
+    return (
+      <div className="blog_post">
+        <img src={this.props.imgSrc} alt="profile picture" />
+        <h2>Full Name :</h2>
+        <p>{this.props.fullname}</p>
+        <h2>Profession :</h2>
+        <p>{this.props.profession}</p>
+        <h2>Bio :</h2>
+
+        <p className="box">{this.props.bio}</p>
+
+        <h1>{this.state.timer}</h1>
+      </div>
+    );
+  }
+}
+
+export default Timer;
